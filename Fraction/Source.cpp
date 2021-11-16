10fb0c5b7fda05eb0bfa5cdd1bd64666e997be8e
Ionina34 <igoninadasha0315@mail.ru>
2021-11-17 00:02:26 +0500

    DZ

---
+++ Fraction/Source.cpp
@@ -0,0 +1,135 @@
+#include<iostream>
+using namespace std;
+
+class Fraction
+{
+	int h; //Числитель
+	int z; //Знаменатель 
+public:
+	int get_h()const
+	{ return h; }
+	int get_z()const
+	{ return z; }
+	void set_h(int h)
+	{ this->h = h; }
+	void set_z(int z)
+	{ this->z = z; }
+	//           Constructors
+	Fraction(int h = 0, int z = 0)
+	{
+		this->h = h;
+		this->z = z;
+		//cout << "Constructor:\t" << this << endl;
+	}
+
+	//          Operator
+	Fraction& operator=(const Fraction& other)
+	{
+		this->h = other.h;
+		this->z = other.z;
+		//cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	Fraction& operator +=( Fraction& other)
+	{
+		if (this->z != other.z)
+		{
+			this->h = (this->h * other.z);
+			other.h =(other.h * this->z);
+			this->h += other.h;
+			this->z *= other.z;
+		}
+		return *this;
+	}
+
+	//          Increment/Decrement
+	Fraction& operator++()
+	{
+		h+=z;
+		return *this;
+	}
+	Fraction& operator++(int)
+	{
+		Fraction old;
+		h+=z;
+		return old;
+	}
+	Fraction& operator--()
+	{
+		h -= z;
+		return *this;
+	}
+	Fraction& operator--(int)
+	{
+		Fraction old;
+		h -= z;
+		return old;
+	}
+
+	//         Methods
+	void print()const
+	{
+		cout << h << "/" <<z << endl;
+	}
+};
+Fraction operator+(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	if (left.get_z() != right.get_z())
+	{
+		result.set_h(left.get_h() * right.get_z() + right.get_h() * left.get_z());
+		result.set_z(left.get_z() * right.get_z());
+		return result;
+	}
+	else
+	{
+		result.set_h(left.get_h() + right.get_h());
+		result.set_z(left.get_z());
+	}
+}
+Fraction operator-(const Fraction& left,const Fraction& right)
+{
+	Fraction result;
+	if (left.get_z() != right.get_z())
+	{
+		result.set_h(left.get_h() * right.get_z() - right.get_h() * left.get_z());
+		result.set_z(left.get_z() * right.get_z());
+	}
+	else
+	{
+		result.set_h(left.get_h() - right.get_h());
+		result.set_z(left.get_z());
+	}
+	return result;
+}
+Fraction operator*(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	result.set_h(left.get_h() * right.get_h());
+	result.set_z(left.get_z() * right.get_z());
+	return result;
+}
+Fraction operator/(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	result.set_h(left.get_h() * right.get_z());
+	result.set_z(left.get_z() * right.get_h());
+	return result;
+}
+ostream& operator<<(ostream& os, const Fraction& obj)
+{
+	os << obj.get_h() << "/" << obj.get_z();
+	return os;
+}
+
+void main()
+{
+	Fraction A(2, 3);
+	A.print();
+	Fraction B(3, 4);
+	B.print();
+	A += B;
+cout << A << endl;
+	//cout << B<< endl;
+	
+}
--- IntroductionToOOP/Source.cpp
+++ IntroductionToOOP/Source.cpp
@@ -25,16 +25,6 @@ public:
 	{
 		this->y = y;
 	}
-	/*double distance(double x,double y)
-	{
-		return sqrt(this->x*this->x  + this->y *this->y);
-	}*/
-
-	/*double distance(const Point& W, const Point& T)
-	{
-		return sqrt((W.get_x() - T.get_x()) * (W.get_x() - T.get_x()) + (W.get_y() - T.get_y()) * (W.get_y() - T.get_y()));
-	}*/
-
 
 	//                       Constructors:
 
@@ -66,31 +56,92 @@ public:
 	}
 
 	//                          Operators
-Point operator=(const Point& other)
+	Point& operator=(const Point& other)
 	{
 		this->x = other.x;
-	    this->y = other.y;
-	  cout << "CopyAssignment:\t" << this << endl;
-	  return *this;
+		this->y = other.y;
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	Point& operator+=(const Point& other)
+	{
+		this->x += other.x;
+		this->y += other.y;
+		return *this;
 	}
 
-		~Point()
+	~Point()
 	{
 		cout << "Destructor:\t" << this << endl;
 	}
 
+	//                      Increment/Decrement
+	Point& operator++() //Prefix Increment
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int)
+	{
+		Point old = *this; //Сохраняем старое значение объекта
+		x++;
+		y++;
+		return old;
+	}
+
 	//                         Methods:
 
+	double distance(const Point& other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+		//sqrt - Square Root (Квадратный корень)
+	}
 	void print()const
 	{
 		cout << "X = " << x << "\tY = " << y << endl;
 	}
 };
+
 double distance(const Point& W, const Point& T);
+double distance_2(const Point& A, const Point& B)
+{
+	return sqrt(pow(A.get_x() - B.get_x(), 2) + (pow(A.get_y() - B.get_y(), 2)));
+}
+
+Point operator+(const Point& left, const Point& right)
+{
+	Point result; //Локальный объект, в который будет сохранен результат
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+Point operator-(const Point& left, const Point& right)
+{
+	Point result(left.get_x() - right.get_x(), left.get_y() - right.get_y());
+	return result;
+}
+Point operator*(const Point& left, const Point& right)
+{
+	return Point(left.get_x() * right.get_x(), left.get_y() * right.get_y());
+}
+
+ostream& operator<<(ostream& os, const Point& obj)
+{
+	os << "X =" << obj.get_x() << "\tY = " << obj.get_y();
+	return os;
+}
 
 //#define STRUCT_POIN
 //#define CONSTRUCTORS_CHECK
-//#define ASSIGMENT_OPERATOR
+//#define ASSIGMENT_CHECK
+//#define DZ_1
+//#define DISTANCE_CHECK
+//#define ARITHMETICAL_OPERATOR_CHECK
+#define delimiter "\n-------------------------------------\n"
 
 
 void main()
@@ -111,6 +162,20 @@ void main()
 	Point* pA = &A;//Создаем (объявляем) указатель на Point
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POIN
+
+#ifdef DISTANCE_CHECK
+	Point A(2, 3);
+	Point B(3, 4);
+	cout << delimiter << endl;
+	cout << "расттояние от точки а до точки в: " << A.distance(B) << endl;
+	cout << delimiter << endl;;
+	cout << "расттояние от точки b до точки a: " << B.distance(A) << endl;
+	cout << delimiter << endl;;
+	cout << "расстояние между точками а и в: " << distance_2(A, B) << endl;
+	cout << delimiter << endl;;
+	cout << "расстояние между точками в и а: " << distance_2(B, A) << endl;
+	cout << delimiter << endl;;
+#endif // DISTANCE_CHECK
 
 #ifdef CONSTRUCTORS_CHECK
 	cout << int() << endl; //Значение поумолчанию для int.
@@ -136,7 +201,7 @@ void main()
 	F.print();
 #endif // CONSTRUCTORS_CHECK
 
-#ifdef ASSIGMENT_OPERATOR
+#ifdef ASSIGMENT_CHECK
 	int a, b, c;
 	a = b = c = 0;
 
@@ -144,8 +209,12 @@ void main()
 	cout << "\n----------------------------------\n";
 	A = B = C = Point(2, 3);
 	cout << "\n----------------------------------\n";
-#endif // ASSIGMENT_OPERATOR
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGMENT_CHECK
 
+#ifdef DZ_1
 	int x;
 	int y;
 	Point T;
@@ -157,7 +226,36 @@ void main()
 	W.set_x(x);
 	W.set_y(y);
 	//cout << "Расстояние до точки: " << distance(W,T) << endl;
-	cout << "Расстояние между точками: " << distance(W, T)<<endl;
+	cout << "Расстояние между точками: " << distance(W, T) << endl;
+#endif // DZ_1
+
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	Point A(2, 3);
+	Point B(3, 4);
+	A += B;
+	A.print();
+	B = A++;
+	/*A.print();
+	B.print();*/
+	cout << A << endl;
+	cout << B << endl;
+
+	/*for (Point i = 0; i.get_x() < 10; i++)
+	{
+		i.print();
+	}*/
+
+#ifdef ARITHMETICAL_OPERATOR_CHECK
+	Point C = A + B;
+	C.print();
+	Point D = A - B;
+	D.print();
+	(A * B).print();
+#endif // ARITHMETICAL_OPERATOR_CHECK
+
 }
 
 double distance(const Point& W, const Point& T)
