3308910658ecc2ff35e74f5b60f3827ebc0cd468
Ionina34 <igoninadasha0315@mail.ru>
2021-11-30 18:19:47 +0500

    MoveMethodsDone

--- String/Source.cpp
+++ String/Source.cpp
@@ -47,6 +47,15 @@ public:
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstractor:" << this << endl;
 	}
+	String(String&& other)
+	{
+		this->size = other.size;
+		this->str = other.str; //Копируем указатель на уже выделенную пямять, принадлежащую другому объекту.
+		other.str = nullptr;   //Зануляем указатель в другом обьекте, 
+		                       //чтобы диструктор не смог удалить память, которая ему принадлежит.
+		other.size = 0;
+		cout << "MoveConstructor:\t" << this << endl;
+	}
 	~String()
 	{
 		delete[] this->str;
@@ -67,6 +76,16 @@ public:
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstractor:" << this << endl;
 		return *this;
+	}
+	String& operator=(String&& other)
+	{
+		if (this == &other)return *this;
+		delete[] this->str;
+		this->size = other.size;
+		this->str = other.str;
+		other.str = nullptr;
+		other.size = 0;
+		cout << "MoveAssighment:\t" << this << endl;
 	}
 	String& operator+=(const String& other)
 	{
@@ -116,7 +135,9 @@ ostream& operator<<(ostream& os, const String& obj)
 	return os << obj.get_str();
 }
 
-//#define CONSTRACTORS_CHECK
+#define CONSTRACTORS_CHECK
+//#define OPERATOR_PLUS_CHECK
+//#define CONSTRACTOR_CALLING
 
 void main()
 {
@@ -144,11 +165,36 @@ void main()
 	cout << a << endl;
 #endif // CONSTRACTORS_CHECK
 
+#ifdef OPERATOR_PLUS_CHECK
 	String str1 = "Hello";
 	String str2 = "World";
-	String str3 = str1 + str2;  //Неявно вызываем оператор + 
+	cout << "\n---------------------------------------\n";
+	String str3;
+	str3 = str1 + str2;  //Неявно вызываем оператор + 
+	cout << "\n---------------------------------------\n";
 	cout << str3 << endl;
-	cout << operator+(str1, str2) << endl;  //Явный вызов оператора+
-	str1 += str2;
+	//cout << operator+(str1, str2) << endl;  //Явный вызов оператора+
+	//str1 += str2;
+	//cout << str1 << endl;  
+#endif // OPERATOR_PLUS_CHECK
+
+#ifdef CONSTRACTOR_CALLING
+	String str1;              //Default constractor
 	cout << str1 << endl;
+	String str2 = "Hello";    //Single-argument constructor
+	cout << str2 << endl;
+	String str3 = str2;       //Copy constructor
+	cout << str3 << endl;
+	str3.print();
+
+	String str4();  //Здесь НЕ создаётся никаклй объкт, 
+					//эта строка НЕ вызывает конструктор по умолчанию явным образом,
+					//Здесь объявляется функция str4(), которая ничего не принимает, 
+	                //и возвращает объкт класса String
+
+   //cout << str4 << endl;
+	String str5{}; //Явный вызов конструктора по умолчанию
+	str5.print();
+#endif // CONSTRACTOR_CALLING
+
 }
