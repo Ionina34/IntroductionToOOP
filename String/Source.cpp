0c10c4d385fd86268042a3c76c9531902b230d17
Ionina34 <igoninadasha0315@mail.ru>
2021-11-30 18:45:46 +0500

    DelagetionDone

--- String/Source.cpp
+++ String/Source.cpp
@@ -30,15 +30,13 @@ public:
 	{
 		cout << "DefConstract:\t" << this << endl;
 	}
-	String(const char* str) :size(strlen(str) + 1), str(new char[size] {})
+	String(const char* str) : String(strlen(str)+1)
 	{
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t" << this << endl;
 	}
-	String(const String& other) :size(other.size), str(new char[size] {})
+	String(const String& other) :String(other.str)
 	{
-		//Deep copy
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstractor:" << this << endl;
 	}
 	String(String&& other):size(other.size),str(other.str)
